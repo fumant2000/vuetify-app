34d0b357e643b7e14cee6ba8752b0a2c1c3c74aa
fumant2000 <erictiobo17@gmail.com>
2024-08-10 05:32:52 +0100

    ugzugz

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-toolbar flat app prominent>
+        <v-app-bar-nav-icon color="grey" @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">Todo</span>
+            <span>SEDS</span>
+        </v-toolbar-title>
+        <v-btn flat color="grey">
+            <span>Sign Out</span>
+            <v-icon
+          icon="mdi-logout"
+          end
+        ></v-icon>
+        </v-btn>
+        <v-navigation-drawer
+        v-model="drawer" app class="blue">
+      </v-navigation-drawer>
+
+    </v-toolbar>
+</template>
+<script >
+export default {
+    data(){
+        return{
+            drawer: false
+        }
+    }
+
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+    <h1>Home Page</h1>
+    <p></p>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+export default{
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
